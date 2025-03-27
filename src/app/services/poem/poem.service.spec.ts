1e4d753318ce448b6abc9261ca66a9b298bad947
Gabriel Bissey <notsureiwantthispublic@email>
2025-03-26 23:50:28 -0600

    Fix unit tests

--- src/app/services/poem/poem.service.spec.ts
+++ src/app/services/poem/poem.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PoemService } from './poem.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PoemService', () => {
   let service: PoemService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(PoemService);
   });
 
